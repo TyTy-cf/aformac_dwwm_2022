ffa40519537ca5c3653c57c90acc4ffc90ba5c23
Kevin Tourret <tourret.kevin@gmail.com>
2022-06-30 09:32:25 +0200

    correction 5

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {IStudent} from "../models/i-student";
 
 @Component({
   selector: 'app-root',
@@ -6,12 +7,32 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+
   title = 'nomprojet';
+  // anArray: Array<string> = new Array<string>();
+  // anArray: Array<string> = [];
+  // anArray: string[] = [];
 
   constructor() {
     // this.exo1(32);
-    this.calcAverage([12, 15, 19, 2, 14]);
-    this.calcTTC(7.5, 4);
+    // this.calcAverage([12, 15, 19, 2, 14]);
+    // this.calcTTC(7.5, 4);
+    const students: IStudent[] =
+    [
+      {
+        name: 'Albert',
+        grades: [12, 15, 19, 2, 14]
+      },
+      {
+        name: 'Vincent',
+        grades: [17, 16, 15, 18, 13]
+      },
+      {
+        name: 'Michel',
+        grades: [14, 13, 12, 11, 10]
+      }
+    ];
+    this.studentsAverage(students);
   }
 
   exo1(age: number): void {
@@ -41,9 +62,21 @@ export class AppComponent {
    * @param taxes
    * @private
    */
-  private calcTTC(priceU: number, qty: number, taxes: number = 19.6): void {
+  calcTTC(priceU: number, qty: number, taxes: number = 19.6): void {
     const price: string = ((priceU * qty) * (taxes / 100 + 1)).toFixed(2);
-    console.log(price);
+    console.log(price + '€');
   }
 
+  studentsAverage(arrayStudents: IStudent[]): void {
+    // for (let i: number = 0; i < students.length; i++) {
+    //   console.log(students[i]);
+    // }
+
+    // je veux accéder à UN seul étudiant de mon tableau
+    // => parcourir le tableau, pour chaque étudiant faire...
+    for (const aStudent of arrayStudents) {
+      // affichage du nom de l'étudiant avec sa moyenne calculée
+      console.log('Etudiant : ' + aStudent.name + ' a ' + this.calcAverage(aStudent.grades));
+    }
+  }
 }
