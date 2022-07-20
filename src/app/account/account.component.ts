006c92999c9b60900005fc74743445cc16535922
Kevin Tourret <kevin@drosalys.fr>
2022-07-20 16:32:04 +0200

    correction exo account

--- src/app/account/account.component.ts
+++ src/app/account/account.component.ts
@@ -3,6 +3,7 @@ import {HttpClientService} from "../../services/http-client.service";
 import {UrlApi} from "../../services/url-api";
 import {Account} from "../../models/steamish/account";
 import {SteamIshRequest} from "../../models/steamish/steam-ish-request";
+import {sprintf} from "sprintf-js";
 
 @Component({
   selector: 'app-account',
@@ -12,13 +13,18 @@ import {SteamIshRequest} from "../../models/steamish/steam-ish-request";
 export class AccountComponent implements OnInit {
 
   steamIshRequest: SteamIshRequest<Account> | undefined;
+  arrayItems: number[] = [10, 15, 20, 25, 50];
 
   constructor(private _httpService: HttpClientService) { }
 
   ngOnInit(): void {
-    this._httpService.getRequest<SteamIshRequest<Account>>(UrlApi.urlAccount)
-      .subscribe((jsonResponse) => {
-        console.log(jsonResponse.items);
+    this.getAccounts();
+  }
+
+  getAccounts(page: number = 1, itemsPerPage: number = 20): void {
+    this._httpService.getRequest<SteamIshRequest<Account>>(sprintf(UrlApi.urlAccountPaginate, page, itemsPerPage))
+    .subscribe((jsonResponse) => {
+        this.steamIshRequest = jsonResponse;
       }
     );
   }
--- src/services/url-api.ts
+++ src/services/url-api.ts
@@ -9,5 +9,6 @@ export class UrlApi {
 
   static rawUrlSteamIsh: string = 'https://steam-ish.test-02.drosalys.net/api/';
   static urlAccount: string = UrlApi.rawUrlSteamIsh + 'account';
+  static urlAccountPaginate: string = UrlApi.urlAccount + '?page=%s&limit=%s';
 
 }
